c519240bd21e883c4bc6ddd7218ae8bc893a9612
KitFisto <kitfist0@yandex.ru>
2021-07-03 15:28:14 +0300

    Release version 0.9.1

--- app/src/main/java/app/flashlight/core/DataStoreManager.kt
+++ app/src/main/java/app/flashlight/core/DataStoreManager.kt
@@ -5,7 +5,6 @@ import androidx.datastore.preferences.core.booleanPreferencesKey
 import androidx.datastore.preferences.core.edit
 import androidx.datastore.preferences.core.intPreferencesKey
 import androidx.datastore.preferences.preferencesDataStore
-import app.flashlight.core.DataConstants
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
 
