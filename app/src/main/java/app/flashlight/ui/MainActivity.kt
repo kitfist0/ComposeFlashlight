a219cb54a5f35438eecd33ba1b7a8d50ee9441fb
kitfist0 <kitfist0@yandex.ru>
2023-04-06 20:51:35 +0300

    Switch theme on button click

--- app/src/main/java/app/flashlight/ui/MainActivity.kt
+++ app/src/main/java/app/flashlight/ui/MainActivity.kt
@@ -3,9 +3,11 @@ package app.flashlight.ui
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
 import androidx.navigation.compose.rememberNavController
 import app.flashlight.ui.theme.ComposeFlashlightTheme
@@ -14,13 +16,17 @@ import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+
+    private val viewModel: MainViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            ComposeFlashlightTheme {
+            val darkTheme = viewModel.darkTheme.collectAsState(initial = false)
+            ComposeFlashlightTheme(darkTheme.value) {
                 Surface(
                     modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
+                    color = MaterialTheme.colorScheme.background,
                 ) {
                     NavGraph(rememberNavController())
                 }
