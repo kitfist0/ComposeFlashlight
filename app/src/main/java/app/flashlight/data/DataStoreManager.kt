66d61c5e59c1afad8d50583467bd7db5aff16075
kitfist0 <kitfist0@yandex.ru>
2024-01-05 22:57:27 +0300

    Add shutdown timeout to data store

--- app/src/main/java/app/flashlight/data/DataStoreManager.kt
+++ app/src/main/java/app/flashlight/data/DataStoreManager.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import androidx.datastore.preferences.core.booleanPreferencesKey
 import androidx.datastore.preferences.core.edit
 import androidx.datastore.preferences.core.intPreferencesKey
+import androidx.datastore.preferences.core.longPreferencesKey
 import androidx.datastore.preferences.preferencesDataStore
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
@@ -30,7 +31,15 @@ class DataStoreManager @Inject constructor(appContext: Context) {
     }
 
     val mode: Flow<Mode> = dataStore.data.map { preferences ->
-        Mode.values()[preferences[MODE_KEY] ?: Mode.DEFAULT_MODE.ordinal]
+        Mode.entries[preferences[MODE_KEY] ?: Mode.DEFAULT_MODE.ordinal]
+    }
+
+    suspend fun setShutdownTimeout(timeout: Long) = dataStore.edit { preferences ->
+        preferences[SHUTDOWN_TIMEOUT_KEY] = timeout
+    }
+
+    val shutdownTimeout: Flow<Long> = dataStore.data.map { preferences ->
+        preferences[SHUTDOWN_TIMEOUT_KEY] ?: Long.MAX_VALUE
     }
 
     suspend fun setDarkThemeEnabled(b: Boolean) = dataStore.edit { preferences ->
@@ -44,6 +53,7 @@ class DataStoreManager @Inject constructor(appContext: Context) {
     private companion object {
         val MODE_KEY = intPreferencesKey("mode")
         val FLASHLIGHT_ENABLED_KEY = booleanPreferencesKey("enabled")
+        val SHUTDOWN_TIMEOUT_KEY = longPreferencesKey("shutdown_timeout")
         val DARK_THEME_KEY = booleanPreferencesKey("dark_theme")
     }
 }
